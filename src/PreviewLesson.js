4bca32ac1697d8851debb4f4c56f683d0866f75b
DarukBey <dorukersoy2006@gmail.com>
2023-09-09 17:08:38 +0300

    fix previewlesson

--- src/PreviewLesson.js
+++ src/PreviewLesson.js
@@ -9,27 +9,32 @@ const PreviewLesson = () => {
     const navigate = useNavigate();
     const [lesson, setLesson] = useState(null);
     const [student, setStudent] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const formatTime = (time) => {
-        const date = new Date(time);
-        return `${String(date.getHours()).padStart(2, '0')}.${String(date.getMinutes()).padStart(2, '0')}`;
+        const d = new Date(time);
+        return `${String(d.getHours()).padStart(2, '0')}.${String(d.getMinutes()).padStart(2, '0')}`;
     };
 
-    const formatDate = (Date) => {
-        const date = new Date(Date);
-        return `${String(date.getMonth() + 1).padStart(2, '0')}.${String(date.getDate()).padStart(2, '0')}.${date.getFullYear()}`;
+    const formatDate = (date) => {
+        const d = new Date(date);
+        return `${String(d.getMonth() + 1).padStart(2, '0')}.${String(d.getDate()).padStart(2, '0')}.${d.getFullYear()}`;
     };
 
     useEffect(() => {
         axios.get(`http://localhost:3001/getStudent/${studentId}`)
-        .then(({ data }) => {
-            const foundLesson = data.lessons.find(item => item._id === lessonsId);
-            setStudent(data);
-            if (foundLesson) {
-                setLesson({...foundLesson, date: formatDate(foundLesson.date), startTime: formatTime(foundLesson.startTime), endTime: formatTime(foundLesson.endTime) });
-            }
-        })
-        .catch(error => console.error(error));
+            .then(({ data }) => {
+                const foundLesson = data.lessons.find(item => item._id === lessonsId);
+                setStudent(data);
+                if (foundLesson) {
+                    setLesson({ ...foundLesson, date: formatDate(foundLesson.date), startTime: formatTime(foundLesson.startTime), endTime: formatTime(foundLesson.endTime) });
+                }
+                setLoading(false); 
+            })
+            .catch(error => {
+                console.error(error);
+                setLoading(false); 
+            });
     }, [studentId, lessonsId]);
 
     const handleDelete = () => {
@@ -44,16 +49,21 @@ const PreviewLesson = () => {
     };
 
     return (
-        <div className="lessonPreview">
-            <h2>{ student.name } { student.surname}</h2>
-            <h3>{lesson.instrument} Lesson</h3>
-            <p className="lessonPreviewp"><b>Date: </b>{lesson.date}</p>
-            <p className="lessonPreviewp"><b>Start Time: </b>{lesson.startTime}</p>
-            <p className="lessonPreviewp"><b>End Time: </b>{lesson.endTime}</p>
-            <p className="lessonPreviewp"><b>Recurring: </b>{lesson.recurring ? "Yes" : "No"}</p>
-            <p className="lessonPreviewp"><b>Status: </b>{lesson.status}</p>
-            <a className="lessonPreviewp" href={`/editLesson/${studentId}/${lessonsId}`}><img src={EditImage} alt="Edit SVG" /></a>
-            <img className="lessonPreviewp" src={DeleteImage} alt="Delete SVG" onClick={() => handleDelete()} />
+        <div>
+            {loading ?
+            ( <p>Loading...</p> ) : (
+            <div className="lessonPreview">
+                <h2>{ student?.name } { student?.surname }</h2>
+                <h3>{lesson?.instrument} Lesson</h3>
+                <p className="lessonPreviewp"><b>Date: </b>{lesson?.date}</p>
+                <p className="lessonPreviewp"><b>Start Time: </b>{lesson?.startTime}</p>
+                <p className="lessonPreviewp"><b>End Time: </b>{lesson?.endTime}</p>
+                <p className="lessonPreviewp"><b>Recurring: </b>{lesson?.recurring ? "Yes" : "No"}</p>
+                <p className="lessonPreviewp"><b>Status: </b>{lesson?.status}</p>
+                <a className="lessonPreviewp" href={`/editLesson/${studentId}/${lessonsId}`}><img src={EditImage} alt="Edit SVG" /></a>
+                <img className="lessonPreviewp" src={DeleteImage} alt="Delete SVG" onClick={() => handleDelete()} />
+            </div>
+            )}
         </div>
     );
 };
