094ab9a59fa2fe90052609e7f1d87a31b8310914
DarukBey <dorukersoy2006@gmail.com>
2023-09-20 15:02:19 +0300

    PDF

--- src/PDF.js
+++ src/PDF.js
@@ -2,8 +2,15 @@ const PDF = () => {
     return (
         <div>
             <h1>PDFs</h1>
-            <div className="pdfLinks">
-                <a href={`${process.env.PUBLIC_URL}/testpdf.pdf`} target="_blank" rel="noopener noreferrer">Download Test PDF</a>
+            <div className="PDFLinks">
+                <a className="PDF" href={`${process.env.PUBLIC_URL}/Book1.pdf`} target="_blank" rel="noopener noreferrer">Download Book 1</a>
+                <a className="PDF" href={`${process.env.PUBLIC_URL}/Book2pdf`} target="_blank" rel="noopener noreferrer">Download Book 2</a>
+                <a className="PDF" href={`${process.env.PUBLIC_URL}/Book3.pdf`} target="_blank" rel="noopener noreferrer">Download Book 3</a>
+                <a className="PDF" href={`${process.env.PUBLIC_URL}/Book4.pdf`} target="_blank" rel="noopener noreferrer">Download Book 4</a>
+                <a className="PDF" href={`${process.env.PUBLIC_URL}/Book5.pdf`} target="_blank" rel="noopener noreferrer">Download Book 5</a>
+                <a className="PDF" href={`${process.env.PUBLIC_URL}/Book6.pdf`} target="_blank" rel="noopener noreferrer">Download Book 6</a>
+                <a className="PDF" href={`${process.env.PUBLIC_URL}/Book7.pdf`} target="_blank" rel="noopener noreferrer">Download Book 7</a>
+                <a className="PDF" href={`${process.env.PUBLIC_URL}/Book8.pdf`} target="_blank" rel="noopener noreferrer">Download Book 8</a>
             </div>
         </div>
      );
